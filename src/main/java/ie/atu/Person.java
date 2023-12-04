f9cb2543372429e024e90d79b7808d24b239916d
NathanMcCormack <G00414780@atu.ie>
2023-12-04 09:18:29 +0000

    new person class created with 3 public strings.

---
+++ src/main/java/ie/atu/Person.java
@@ -0,0 +1,40 @@
+package ie.atu;
+
+public class Person {
+    public String name;
+    public String address;
+    public String phone;
+
+    public Person() { //default constructor
+    }
+
+    public Person(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
